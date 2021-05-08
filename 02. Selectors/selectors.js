8512b6db77cc29abcd0c553901898d5d754564fb
fuadhasanemon <fuadhasanemon8@gmail.com>
2021-05-09 00:31:57 +0600

    child selectors

--- 02. Selectors/selectors.js
+++ 02. Selectors/selectors.js
@@ -61,3 +61,37 @@ console.log("hello selectors.js")
 // $(document).ready(function(){
 //   $("p:last-child").css("background", "yellow");
 // });
+
+// 11 all :last-of-type selector
+
+// $(document).ready(function(){
+//   $("p:last-of-type").css("background", "yellow");
+// });
+
+// 12 all :nth-child(n) selector
+
+// $(document).ready(function(){
+//     $("p:nth-child(2)").css("background", "pink");
+//   });
+
+
+// 13 all :nth-last-child(n) selector
+
+// $(document).ready(function(){
+//     $("p:nth-last-child(1)").css("background", "pink");
+//   });  
+
+
+
+// 14 all :nth-last-of-type(n) selector
+
+// $(document).ready(function(){
+//     $("p:nth-last-of-type(1)").css("background", "pink");
+//   });  
+
+
+// 15 all :only-child selector
+
+$(document).ready(function(){
+    $("p:only-child").css("background", "orange");
+  });  
